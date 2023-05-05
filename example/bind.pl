027360a7892ba88c189a675718515fa2f727f4d7
Max Maischein <corion@corion.net>
2023-05-05 16:43:39 +0200

    Test binding subroutines

--- example/bind.pl
+++ example/bind.pl
@@ -4,6 +4,7 @@ use feature 'signatures';
 no warnings 'experimental::signatures';
 
 use threads;
+use Thread::Queue;
 use UI::Webview 'WEBVIEW_HINT_NONE';
 
 our $called;
@@ -20,26 +21,30 @@ my $html = <<'HTML';
 </script>
 HTML
 
+my $q = Thread::Queue->new;
+
 my @ctx;
-#my $ui_thread = threads->create(sub {
+my $ui_thread = threads->create(sub($q) {
     state $w = UI::Webview::webview_create(1,undef);
     UI::Webview::webview_set_title($w, "Perl Example");
     UI::Webview::webview_set_size($w, 480, 320, WEBVIEW_HINT_NONE);
     push @ctx, UI::Webview::webview_bind($w, "perl_callback" => sub($w,$seq,$req) { say "In Perl (anonymous)"; say "$seq: $req"; $called++ }, []);
     push @ctx, UI::Webview::webview_bind($w, "stop_webview" => sub($w,$seq,$req) { UI::Webview::webview_terminate($w) }, []);
     UI::Webview::webview_set_html($w, $html);
+    $q->enqueue($w);
     UI::Webview::webview_run($w);
-    # XXX we want to unbind our callback(s) here before trying to exit
+    # XXX we want to unbind our callback(s) here before trying to exit,
+    #     but unbinding doens't really do much in the Webview anyway
 #    UI::Webview::webview_unbind($w, $ctx);
     UI::Webview::webview_destroy($w);
     say "Done";
-
-#});
+}, $q);
 
 #sleep 2;
 #
-#$| = 1;
-#while(1) {
-#    sleep 1;
-#    printf "%s , called %d times\r", scalar(localtime), $called;
-#}
+my $w = $q->dequeue;
+$| = 1;
+sleep 2;
+printf "%s , called %d times\r", scalar(localtime), $called;
+#UI::Webview::webview_terminate($w);
+$ui_thread->join;
